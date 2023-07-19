8f67b6622a4334d02e64543aee72f6d40565f3e8
Ildefonso Zanette <i.z@uol.com.br>
2023-07-19 20:26:52 -0300

    used the new GetLastestNewData method

--- Assets/Scripts/VideoManagerReceiver.cs
+++ Assets/Scripts/VideoManagerReceiver.cs
@@ -22,8 +22,8 @@ public class VideoManagerReceiver : MonoBehaviour
 
     void PlayVideo()
     {
-        string data = udpReceiver.GetLastestData();
-        //string data = udpReceiver.GetLastestNewData(1.0f);// don't get data that is older than 1 second
+        //string data = udpReceiver.GetLastestData();
+        string data = udpReceiver.GetLastestNewData(1.0f);// don't get data that is older than 1 second
         int value;
         if (int.TryParse(data, out value))
         {
